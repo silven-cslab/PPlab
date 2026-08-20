7a0138d20ecf926622651bae3b0c1ac6d8d1d27e
silven-cslab <silvencslab@gmail.com>
2026-08-20 10:36:42 +0530

    A brand new day.

--- 06-08-2026/isprime.py
+++ 06-08-2026/isprime.py
@@ -13,12 +13,23 @@ if num == 0 or num == 1:
     print(f"{num} is nether prime nor composite.")
     exit()
 
+
 def isPrime(num):
-    for i in range(2, int(num/2 + 1)):
-        if num % i == 0:
+    SQRTnum = int(num ** 0.5)
+    for PRIMES in PRIMESLIST:
+        if PRIMES > SQRTnum:
+            return True
+        if num % PRIMES == 0:
             return False
 
     return True
+
+PRIMESLIST = []
+SQRTnum = int(num ** 0.5)
+
+for s in range(2, SQRTnum + 1):
+    if isPrime(s):
+        PRIMESLIST.append(s)
 
 if isPrime(num):
     print(f"{num} is a Prime Number.")
